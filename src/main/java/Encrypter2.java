6e4b64c8189afa055732f30a78a2ce2e7e8199d7
Niels Engelhard <N.Engelhard@student.han.nl>
2019-12-06 16:09:19 +0100

    Added files

---
+++ src/main/java/Encrypter2.java
@@ -0,0 +1,59 @@
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.AlgorithmParameters;
+import java.security.spec.KeySpec;
+import java.util.Base64;
+
+public class Encrypter2 {
+
+    private static final String key = "aesEncryptionKey";
+    private static final String initVector = "encryptionIntVec";
+
+    public static String encrypt(String value) {
+        try {
+            IvParameterSpec iv = new IvParameterSpec(initVector.getBytes("UTF-8"));
+            SecretKeySpec skeySpec = new SecretKeySpec(key.getBytes("UTF-8"), "AES");
+
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
+            cipher.init(Cipher.ENCRYPT_MODE, skeySpec, iv);
+
+            byte[] encrypted = cipher.doFinal(value.getBytes());
+
+            byte[] encryptedBytes = Base64.getEncoder().encode(encrypted);
+            String encryptedString = new String(encryptedBytes, "UTF-8");
+
+            return encryptedString;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return null;
+    }
+
+    public static String decrypt(String encrypted) {
+        try {
+            IvParameterSpec iv = new IvParameterSpec(initVector.getBytes("UTF-8"));
+            SecretKeySpec skeySpec = new SecretKeySpec(key.getBytes("UTF-8"), "AES");
+
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
+            cipher.init(Cipher.DECRYPT_MODE, skeySpec, iv);
+            byte[] original = cipher.doFinal(Base64.getDecoder().decode(encrypted));
+
+            return new String(original);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+        String s = encrypt("message");
+        System.out.println(s);
+        System.out.println(decrypt(s));
+    }
+
+}
---
+++ src/test/java/EncrypterTest.java
@@ -0,0 +1,8 @@
+import org.junit.Test;
+
+public class EncrypterTest {
+
+    @Test
+    public void testEncrypter() throws Exception {
+    }
+}
